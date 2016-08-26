91234c3fa50abe9261dde20a5a3ec624769a2191
Diego Legua <Diego@macbook-pro-skullone.local>
2016-08-26 11:04:26 +0200

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Diego Legua on 26/08/16.
+//  Copyright © 2016 DL.Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
